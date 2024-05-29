49f83831c36ed6bd132b12ca69c73b6d3d38a8b0
Vaibhav Kumar <vaibhavpro9210@gmail.com>
2024-05-29 16:43:40 +0530

    Starting fresh with Vite for project setup

--- backend/routes/problemRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const problemController = require('../controllers/problemController');
-
-router.post('/', problemController.createProblem);
-router.get('/', problemController.getProblems);
-router.get('/:id', problemController.getProblemById);
-
-module.exports = router;
--- backend/routes/submissionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const submissionController = require('../controllers/submissionController');
-
-router.post('/', submissionController.createSubmission);
-router.get('/', submissionController.getSubmissions);
-
-module.exports = router;
